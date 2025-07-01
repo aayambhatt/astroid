bfd0c23b16aaab8f9d46ead31343b6ea81067e3e
aayambhatt <aayambhatt23@gmail.com>
2025-07-01 16:25:00 +0530

    Migrate to uv for project management

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -9,13 +10,28 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    clock = pygame.time.Clock()  # Regulates FPS
+    dt = 0  # Delta time (time between frames)
     # Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return  # Exit game loop
+        
+        player.update(dt) 
+            
         screen.fill((0,0,0)) # set screen to solid black
+        player.draw(screen) # Draw the player
+
         pygame.display.flip() # Refresh the screen
+
+        dt = clock.tick(60) / 1000  # Limit to 60 FPS, get delta time in seconds
+
+        
+
+        
 
 if __name__ == "__main__":
     main()
